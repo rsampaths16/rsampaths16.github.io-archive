0fa6f8d49a071e7e5fe63719ef23564f5574817b
Sampath <rsampaths16@gmail.com>
2021-03-24 00:01:42 +0530

    Add a filter to join list in english
    Registered a custom list filter to join list
    as comma separated english string
    
    ([a], singluar-prefix, plural-prefix)
        => "singluar-prefix" + "a"
    
    ([a, b ..., z], singular-prefix, plural-prefix)
        => "plural-prefix" + "a, b, ... and z"

---
+++ _plugins/english_list.rb
@@ -0,0 +1,32 @@
+module Jekyll
+    module ListFilter
+        def english_list(input, single_element_prefix="", multiple_element_prefix="")
+            if !input.kind_of?(Array)
+                raise ArgumentError, 'Input to filter english_list is not a list'
+            end
+
+            input_length = input.length
+            output = ""
+
+            if input_length == 1
+                output += single_element_prefix
+            elsif input_length > 1
+                output += multiple_element_prefix
+            end
+
+            input.each_with_index do |value, index|
+                if index == 0
+                    output += value
+                elsif index == (input_length-1)
+                    output += " and " + value
+                else
+                    output += ", " + value
+                end
+            end
+
+            return output
+        end
+    end
+end
+
+Liquid::Template.register_filter(Jekyll::ListFilter)
